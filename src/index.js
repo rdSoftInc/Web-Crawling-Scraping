37c9ff6ee5a86137445fc12e0a83354ef96faede
simple402 <greatgoals1503@protonmail.com>
2023-10-05 23:00:27 +0200

    Connected Firebase, set up saving info, and refactored.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,33 @@
-
-// imported inbuilt packages...
-
-import * as fs from 'fs'
-
-// user defined functions...
-
-import BaseUrl from './base-url.js'
-
-import Crawler from './crawler.js'
-
-import StoreUrlsLocal from './store-urls-local.js'
-
-// fetching the initial urls to proceed...
-
-const storedUrls = JSON.parse(fs.readFileSync('prerequisites/urls.json', 'utf-8')).urls
-
-// start of the application...
+import * as fs from 'fs';
+import BaseUrl from './base-url.js';
+import Crawler from './crawler.js';
+import StoreUrlsLocal from './store-urls-local.js';
+import processLinks from './scraper.js';
+import StoreDataLocal from './store-data-local.js';  // import the local storage function
 
 async function main() {
-
     console.log('\nCrawling started...\n');
 
+    const storedUrls = JSON.parse(fs.readFileSync('prerequisites/urls.json', 'utf-8')).urls;
+
     for (const url of storedUrls) {
-
-        // function to fetch base url of the link from prerequistes/urls.json file for links with no base url...
-
         const baseUrl = BaseUrl(url);
-
         try {
-
-            // calling crawler function...
-
             const urls = await Crawler(baseUrl, url, 0);
-
-            // storing the found urls to prerequistes/horizon.json file for data extraction...
-
-            await StoreUrlsLocal(urls);           
-
-            
-
+            await StoreUrlsLocal(urls);
         } catch (e) {
-
             console.error('Failed to crawl ' + e);
-
         }
     }
 
     console.log('Crawling finished!\n');
+    console.log('\nScraping started...\n');
+
+    const horizonUrls = JSON.parse(fs.readFileSync('prerequisites/horizon.json', 'utf-8')).seeds;
+    const scrapedData = await processLinks(horizonUrls); // Call the scraper function
+
+    await StoreDataLocal(scrapedData); // Store the scraped data locally
+    console.log('Scraping finished!\n');
 }
 
-main()
-
-
-
+main();
